a16ce6742589509bd4b0dad00c1de33df716bafb
unknown <kkavya0909@gmail.com>
2025-12-03 11:48:52 +0530

    changed driver time to 50sec

--- fashion/src/test/java/com/ecommerce/FirstClassTest.java
+++ fashion/src/test/java/com/ecommerce/FirstClassTest.java
@@ -16,7 +16,7 @@ public class FirstClassTest {
 		
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
 		driver.get("https://www.amazon.in");
 		driver.manage().window().maximize();
 
